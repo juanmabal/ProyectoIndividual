2730f10c6b31d002903a531dee11bb1c2d6326f9
juanmabal <juanete3244@hotmail.com>
2022-09-09 13:46:41 -0300

    Aún con problemas de condiciones al agregar estrellas, ademas de un codigo  deficiente

--- js/product-info.js
+++ js/product-info.js
@@ -1,13 +1,13 @@
-function showCategoriesList(){
+function showCategoriesList() {
 
-    for (let i; i < currentProductArray.images.length ; i++) {
+    for (let i; i < currentProductArray.images.length; i++) {
         let image = currentProductArray.images[i];
-        console.log(image[1]);      
-        document.getElementById("container").innerHTML = `<img src="${image}" class="img-thumbnail">`;   
+        console.log(image[1]);
+        document.getElementById("container").innerHTML = `<img src="${image}" class="img-thumbnail">`;
     }
 
     document.getElementById("container").innerHTML =
-     `<h3 id="product-name">${currentProductArray.name}</h3>
+        `<h3 id="product-name">${currentProductArray.name}</h3>
      <hr>
       <p class="attributes">Precio</p>
       <p>UYU ${currentProductArray.cost}</p>
@@ -23,22 +23,54 @@ function showCategoriesList(){
       <img class ="images" src="${currentProductArray.images[2]}">
       <img class ="images" src="${currentProductArray.images[3]}">
       `
-      
+
 }
 
 function showCommentsList() {
 
-    let stars = `${currentProductCommentsArray[0].score}`;
-    console.log(stars);
-
-    document.getElementById("container").innerHTML += 
-    `<h4 class="commentaries-title ">Comentarios</h4>
+    document.getElementById("container").innerHTML +=
+        `<h4 class="commentaries-title ">Comentarios</h4>
     <div class="comment_container">
     <p>${currentProductCommentsArray[0].user}</p>
-    <p>${currentProductCommentsArray[0].dateTime}</p>
+    <p>${currentProductCommentsArray[0].dateTime} - </p>
     <p>${currentProductCommentsArray[0].description}</p>
-    </div>
-    <div class="comment_container">
+    </div>`;
+
+    let nochecked_stars = parseInt(`${currentProductCommentsArray[3].score}`); //Estrellas negativas
+    let original_nocheckedstars = nochecked_stars; //Estrellas negativas originales
+
+    let checkedstars = 5 - nochecked_stars; //Estrellas positivas
+
+    while (nochecked_stars < 5) {
+        nochecked_stars += 1;
+        const spanhijo = document.createElement("span")
+        spanhijo.className += "fa fa-star";
+        document.getElementsByTagName("p")[10].insertAdjacentElement("beforeend", spanhijo);
+    }
+
+    console.log(checkedstars)
+    console.log(original_nocheckedstars)
+
+    if (currentProductCommentsArray[3].score == 5) {
+        while (checkedstars < original_nocheckedstars) {
+            console.log("aa")
+            const spanhija = document.createElement("span")
+            spanhija.className += "fa fa-star checked";
+            document.getElementsByTagName("p")[10].insertAdjacentElement("beforeend", spanhija);
+            checkedstars += 1;
+        } 
+    } else {
+        
+        while (checkedstars <= original_nocheckedstars) {
+            console.log("bb")
+            const spanhija = document.createElement("span")
+            spanhija.className += "fa fa-star checked";
+            document.getElementsByTagName("p")[10].insertAdjacentElement("beforeend", spanhija);
+            checkedstars += 1;
+        } 
+    }
+
+    document.getElementById("container").innerHTML += `<div class="comment_container">
     <p>${currentProductCommentsArray[1].user}</p>
     <p>${currentProductCommentsArray[1].dateTime}</p>
     <p>${currentProductCommentsArray[1].description}</p>
@@ -54,27 +86,48 @@ function showCommentsList() {
     <p>${currentProductCommentsArray[3].description}</p>
     </div>`
 
+
+    {/* <p>${currentProductCommentsArray[0].description}</p>
+    </div>
+    <div class="comment_container">
+    <p>${currentProductCommentsArray[1].user}</p>
+    <p>${currentProductCommentsArray[1].dateTime}</p>
+    <p>${currentProductCommentsArray[1].description}</p>
+    </div>
+    <div class="comment_container">
+    <p>${currentProductCommentsArray[2].user}</p>
+    <p>${currentProductCommentsArray[2].dateTime}</p>
+    <p>${currentProductCommentsArray[2].description}</p>
+    </div>
+    <div class="comment_container">
+    <p>${currentProductCommentsArray[3].user}</p>
+    <p>${currentProductCommentsArray[3].dateTime}</p>
+    <p>${currentProductCommentsArray[3].description}</p>
+    </div>` */}
+
+
+
     let parrafo = document.getElementsByTagName("p")[11];
     console.log(parrafo);
 
     const spanhijo = document.createElement("span")
     spanhijo.className += "fa fa-star checked";
-    document.getElementsByTagName("p")[11].insertAdjacentElement("beforebegin", spanhijo);
-    
+    /* document.getElementsByTagName("p")[10].insertAdjacentElement("beforeend", spanhijo); */
+
     const spanhijoo = document.createElement("span")
     spanhijoo.className += "fa fa-star checked";
-    document.getElementsByTagName("p")[11].insertAdjacentElement("beforebegin", spanhijoo);
+    /* document.getElementsByTagName("p")[11].insertAdjacentElement("beforebegin", spanhijoo); */
 
 }
 
-document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(`${PRODUCT_INFO_URL}${prodID}${EXT_TYPE}`).then(function(resultObj){
-        if (resultObj.status === "ok"){
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(`${PRODUCT_INFO_URL}${prodID}${EXT_TYPE}`).then(function (resultObj) {
+        if (resultObj.status === "ok") {
             currentProductArray = resultObj.data
             showCategoriesList();
 
-            getJSONData(`${PRODUCT_INFO_COMMENTS_URL}${prodID}${EXT_TYPE}`).then(function(resultObj){
-                if (resultObj.status === "ok"){
+            getJSONData(`${PRODUCT_INFO_COMMENTS_URL}${prodID}${EXT_TYPE}`).then(function (resultObj) {
+                if (resultObj.status === "ok") {
                     currentProductCommentsArray = resultObj.data
                     showCommentsList();
                 }
@@ -84,7 +137,7 @@ document.addEventListener("DOMContentLoaded", function(e){
     });
 
     document.getElementById("btnEnviar").addEventListener("click", function () {
-        
+
         //Fecha y Hora
         let hoy = new Date();
         let dia = hoy.getDate();
@@ -97,9 +150,9 @@ document.addEventListener("DOMContentLoaded", function(e){
         let fecha = `${anio + '-' + mes + '-' + dia + ' ' + hora + ':' + minutos + ':' + segundos}`
         let user = localStorage.getItem('usuario');
         let comentario = document.getElementById("comentario").value;
-        
+
         document.getElementById("container").innerHTML +=
-        `<div class="comment_container">
+            `<div class="comment_container">
         <p>${user}</p>
         <p>${fecha}</p>
         <p>${comentario}</p>
