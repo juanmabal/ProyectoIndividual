56ec3c58184034cc6083e9e24db2b2d64b79175b
juanmabal <juanete3244@hotmail.com>
2022-09-13 14:45:31 -0300

    Cree función para agregar estrellas ahorrando código

--- js/product-info.js
+++ js/product-info.js
@@ -1,4 +1,23 @@
 let nump = 11; //Número de párrafo que siempre corresponderá a la fecha y que en cuyo final se agregarán estrellas
+let stars = undefined; //Estrellas "encendidas"
+let nostars = undefined; //Estrellas "apagadas"
+
+function addStars() {
+    while (nostars < 5) {
+        const estrella = document.createElement("span")
+        estrella.className += "fa fa-star checked"; //Estrella encendida
+        document.getElementsByTagName("p")[nump].insertAdjacentElement("beforeend", estrella);
+        nostars += 1;
+    }
+
+    while (stars < 5) {
+        const estrella = document.createElement("span") 
+        estrella.className += "fa fa-star"; //Estrella apagada
+        document.getElementsByTagName("p")[nump].insertAdjacentElement("beforeend", estrella);
+        stars += 1;
+    }
+}
+
 
 function showInfoList() { //Información principal: Precio - Descripción - Categoría - Vendidos
 
@@ -20,16 +39,15 @@ function showInfoList() { //Información principal: Precio - Descripción - Cate
         let image = currentProductArray.images[i];
         document.getElementById("container").innerHTML += `<img src="${image}" class="images">`;
     }
-
 }
 
 function showCommentsList() { //Lista de comentarios
 
     document.getElementById("container").innerHTML += `<p class="commentaries-title">Comentarios</p>`
-
+    
     for (let i = 0; i < currentProductCommentsArray.length; i++) {
-
         let currentcomment = currentProductCommentsArray[i];
+        
         document.getElementById("container").innerHTML += `
         <div class="comment_container">
         <p>${currentcomment.user}</p>
@@ -37,28 +55,11 @@ function showCommentsList() { //Lista de comentarios
         <p>${currentcomment.description}</p>
         </div>`;
 
-        let stars = parseInt(`${currentcomment.score}`); //Estrellas "encendidas"
-        let nostars = 5 - stars; //Estrellas "apagadas"
-
-        while (nostars < 5) {
-            const estrella = document.createElement("span")
-            estrella.className += "fa fa-star checked"; //Estrella encendida
-            document.getElementsByTagName("p")[nump].insertAdjacentElement("beforeend", estrella);
-            nostars += 1;
-        }
-
-        while (stars < 5) {
-            const estrella = document.createElement("span") 
-            estrella.className += "fa fa-star"; //Estrella apagada
-            document.getElementsByTagName("p")[nump].insertAdjacentElement("beforeend", estrella);
-            stars += 1;
-        }
-
-        nump += 3; //Cada tres párrafos se encuentra el correspondiente a la fecha
-        
+         stars = parseInt(`${currentcomment.score}`); //Estrellas "encendidas"
+         nostars = 5 - stars; //Estrellas "apagadas"
+         addStars();
+         nump += 3; //Cada tres párrafos se encuentra el correspondiente a la fecha 
     }
-
-
 }
 
 document.addEventListener("DOMContentLoaded", function (e) {
@@ -77,10 +78,9 @@ document.addEventListener("DOMContentLoaded", function (e) {
         }
     });
     
-     //Párrafo correspondiente a la fecha del primer comentario agregado
-    document.getElementById("btnEnviar").addEventListener("click", function () { //Enviar comentario
+    document.getElementById("btnEnviar").addEventListener("click", function () { //Botón enviar
 
-        if (localStorage.getItem("usuario")) { //Si el usuario inicio sesión...
+        if (localStorage.getItem("usuario")) { //Si el usuario ha iniciado sesión...
             let hoy = new Date();
             //Fecha
             let dia = hoy.getDate();
@@ -103,36 +103,20 @@ document.addEventListener("DOMContentLoaded", function (e) {
             <p>${comentario}</p>
             </div>`;
     
-            let stars = undefined; //Estrellas "encendidas"
-            let nostars = undefined //Estrellas "apagadas"
-    
-            stars = parseInt(document.getElementById("puntuacion").value); //Puntuación
-            nostars = 5 - stars;
-    
-            while (nostars < 5) {
-                const estrella = document.createElement("span")
-                estrella.className += "fa fa-star checked"; //Estrella encendida
-                document.getElementsByTagName("p")[nump].insertAdjacentElement("beforeend", estrella);
-                nostars += 1;
-            }
-    
-            while (stars < 5) {
-                const estrella = document.createElement("span") 
-                estrella.className += "fa fa-star"; //Estrella apagada
-                document.getElementsByTagName("p")[nump].insertAdjacentElement("beforeend", estrella);
-                stars += 1;
-            }
+             stars = parseInt(document.getElementById("puntuacion").value); //Puntuación y Estrellas "Encendidas"
+             nostars = 5 - stars; //Estrellas "apagadas"
+             addStars();
 
+            document.getElementById("comentario").value = ""; //Vacía comentario
+            document.getElementById("comentario").disabled = true; //Inhabilita comentario
+            document.getElementById("puntuacion").disabled = true; //Inhabilita puntuación
             document.getElementById("btnEnviar").disabled = true; //Inhabilita botón
         
-        } else { //Si el usuario no inicio sesión..
+        } else { //Si el usuario no ha iniciado sesión..
             alert("Debe iniciar sesión para comentar");
             window.location = "index.html";
         }
-
-       
-              
+            
     })
-
 
 });
