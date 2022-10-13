f2224c04a8c4ce748a7acd38770dcfea8716b30e
juanmabal <juanete3244@hotmail.com>
2022-10-13 14:20:30 -0300

    Subtotal funcionando en tiempo real, aún falta dirección y diseño responsive

--- js/cart.js
+++ js/cart.js
@@ -1,9 +1,44 @@
+let unitCost = undefined;
+
+function newSubtotal() {
+  let cantProduct = document.getElementById("cant_product").value;
+  let subtotal = unitCost * cantProduct; 
+  document.getElementById("subtotal").innerHTML = `USD ` + subtotal;
+}
+
 function showCart() {
     
-    document.getElementById("container").innerHTML += 
-    `<h1>Carrito de compras</h1>
-    <h2>Artículos a comprar</h2>`
-    
+    document.getElementById("cart-container").innerHTML += 
+    `<h2>Carrito de compras</h2>
+    <h4>Artículos a comprar</h4>
+    <div class="cart-info">
+        <div>
+          <p></p>
+          <img src="${currentProductArray.articles[0].image}" alt="">
+        </div>
+        <div>
+          <p>Nombre</p>
+          <p>${currentProductArray.articles[0].name}</p>
+        </div>
+        <div>
+          <p>Costo</p>
+          <p>${unitCost}</p>
+        </div>
+        <div>
+          <p>Cantidad</p>
+            <input oninput="newSubtotal()" id="cant_product" type="number" value="1"></input>
+        </div>
+        <div>
+          <p>Subtotal</p>
+          <p id="subtotal">USD 15200</p>
+        </div>
+        <h4>Tipo de envío</h4>
+        <input type="radio">Premium - 2 a 5 días (15%)
+        <input type="radio">Express - 5 a 8 días (7%)
+        <input type="radio">Standard - 12 a 15 días (5%)
+      </div>
+      `
+
 }
 
 document.addEventListener("DOMContentLoaded", function (e) {
@@ -11,7 +46,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
 getJSONData(`${CART_INFO_URL}25801${EXT_TYPE}`).then(function (resultObj) {
     if (resultObj.status === "ok") {
         currentProductArray = resultObj.data
-        console.log(currentProductArray);
+        unitCost = `${currentProductArray.articles[0].unitCost}`;
         showCart();
         }
     });
