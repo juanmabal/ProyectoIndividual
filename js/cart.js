5dd5d5bdd9f28a814aa02e1ba0730be6d563e34e
juanmabal <juanete3244@hotmail.com>
2022-10-14 23:52:22 -0300

    No pude hacerlo responsive con grid así que implemente tablas. Se ve muy parecido al diseño del ejemplo aunque no estoy seguro de si se puede considerar responsive.

--- js/cart.js
+++ js/cart.js
@@ -9,7 +9,44 @@ function newSubtotal() {
 function showCart() {
     
     document.getElementById("cart-container").innerHTML += 
-    `<h2>Carrito de compras</h2>
+    `<h1>Carrito de compras</h1>
+    <h2>Artículos a comprar</h2>
+    <div class="table-responsive">
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col"></th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Costo</th>
+            <th scope="col">Cantidad</th>
+            <th scope="col">Subtotal</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row"><img src="${currentProductArray.articles[0].image}" alt=""></th>
+            <td>${currentProductArray.articles[0].name}</td>
+            <td>USD ${unitCost}</td>
+            <td><input oninput="newSubtotal()" id="cant_product" value="1"></td>
+            <td id="subtotal">USD ${unitCost}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <h2>Tipo de envío</h2>
+      <div><input type="radio">Premium - 2 a 5 días (15%)</div>
+      <div><input type="radio">Express - 5 a 8 días (7%)</div>
+      <div><input type="radio">Standard - 12 a 15 días (5%)</div>
+      <h2>Dirección de envío</h2>
+      <p>Calle</p>
+      <input type="text"></input>
+      <p>Número</p>
+      <input type="text"></input>
+      <p>Dirección</p>
+      <input type="text"></input>
+    `
+
+   /*  `<h2>Carrito de compras</h2>
     <h4>Artículos a comprar</h4>
     <div class="cart-info">
         <div>
@@ -37,7 +74,7 @@ function showCart() {
         <input type="radio">Express - 5 a 8 días (7%)
         <input type="radio">Standard - 12 a 15 días (5%)
       </div>
-      `
+      ` */
 
 }
 
