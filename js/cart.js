302eb744134806d0ed8f125eaea312c48e767839
juanmabal <juanete3244@hotmail.com>
2022-10-22 14:37:13 -0300

    Todos los subtotales se modifican en tiempo real, falta mostrarlos en dolares

--- js/cart.js
+++ js/cart.js
@@ -2,6 +2,9 @@ let subtotal = undefined;
 let unitCost = undefined;
 let cantArticles = undefined;
 let currency = undefined;
+let subtotalpremium = undefined;
+let subtotalexpress = undefined;
+let subtotalstandard = undefined;
 
 function addToCart() {
 
@@ -32,9 +35,23 @@ function addToCart() {
   document.getElementById(artSubtotal).innerHTML = `${currency} ` + subtotal;
 } */
 
-function envio() {
-  subtotal = ((subtotal * 15) / 100);
-  document.getElementById("subtotal2").innerHTML = subtotal;
+
+function enviopremium() {
+  subtotalpremium = ((subtotal * 15) / 100);
+  document.getElementById("subtotal2").innerHTML = subtotalpremium;
+  document.getElementById("subtotal3").innerHTML = subtotal + subtotalpremium;
+}
+
+function envioexpress() {
+  subtotalexpress = ((subtotal * 7) / 100);
+  document.getElementById("subtotal2").innerHTML = subtotalexpress;
+  document.getElementById("subtotal3").innerHTML = subtotal + subtotalexpress;
+}
+
+function enviostandard() {
+  subtotalstandard = ((subtotal * 5) / 100);
+  document.getElementById("subtotal2").innerHTML = subtotalstandard;
+  document.getElementById("subtotal3").innerHTML = subtotal + subtotalstandard;
 }
 
 function newSubtotal() {
@@ -46,6 +63,16 @@ function newSubtotal() {
 
   document.getElementById("subtotal").innerHTML = currency +` `+ subtotal;
   document.getElementById("subtotal1").innerHTML = currency +` `+ subtotal;
+
+  if (document.getElementById("premium").checked) {
+    enviopremium();
+    /* document.getElementById("subtotal3").innerHTML = subtotal + subtotalpremium; */
+  } else if (document.getElementById("express").checked) {
+    envioexpress();
+    /* document.getElementById("subtotal3").innerHTML = subtotal + subtotalexpress; */
+    } else if (document.getElementById("standard").checked) {
+      enviostandard();
+    }
 }
 
 function showCart() {
@@ -69,7 +96,7 @@ function showCart() {
             <td scope="row"><img src="${currentArticleArray.articles[0].image}" alt=""></td>
             <td>${currentArticleArray.articles[0].name}</td>
             <td>${currentArticleArray.articles[0].currency +` `+ currentArticleArray.articles[0].unitCost}</td>
-            <td><input type="number" oninput="newSubtotal(), envio()" id="cant_articles" min="1"
+            <td><input type="number" oninput="newSubtotal()" id="cant_articles" min="1"
              onkeypress="return (event.charCode >= 48 && event.charCode <= 57)"></td>
             <td id="subtotal">${currentArticleArray.articles[0].currency +` `+ currentArticleArray.articles[0].unitCost}</td>
           </tr>
@@ -77,9 +104,9 @@ function showCart() {
       </table>
     </div>
     <h2>Tipo de envío</h2>
-      <div><input oninput="enviopremium() "name="p" value="p" type="radio">Premium - 2 a 5 días (15%)</div>
-      <div><input oninput="envioexpress() "name="e" value="e" type="radio">Express - 5 a 8 días (7%)</div>
-      <div><input oninput="enviostandard() "name="s" value="s" type="radio">Standard - 12 a 15 días (5%)</div>
+      <div><input oninput="enviopremium()" id="premium" name="envio" value="p" type="radio">Premium - 2 a 5 días (15%)</div>
+      <div><input oninput="envioexpress()" id="express" name="envio" value="e" type="radio">Express - 5 a 8 días (7%)</div>
+      <div><input oninput="enviostandard()" id="standard" name="envio" value="s" type="radio">Standard - 12 a 15 días (5%)</div>
       <h2>Dirección de envío</h2>
       <p>Calle</p>
       <input type="text"></input>
@@ -95,10 +122,9 @@ function showCart() {
       <p>Costo de envío</p><p id="subtotal2">USD</p><p>Según el tipo de envío</p></div>
       <div>
       <div class="subtotales">
-      <p>Total ($)</p><p>USD</p><p>Subtotal</p></div>
+      <p>Total ($)</p><p id="subtotal3">USD</p><p>Subtotal</p></div>
       <div>`
 
-      console.log(p);
       
 }
 
