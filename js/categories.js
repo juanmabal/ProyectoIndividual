a060dcee091414a8503c791a3a8e36bb557a58bc
juanmabal <juanete3244@hotmail.com>
2023-07-06 15:07:33 -0300

    Corrijo error en categorías que generaba que se viera mal la categoria electrodomesticos en pantallas pequeñas

--- js/categories.js
+++ js/categories.js
@@ -62,15 +62,15 @@ function showCategoriesList(){
             htmlContentToAppend += `
             <div onclick="setCatID(${category.id})" class="list-group-item list-group-item-action cursor-active">
                 <div class="row">
-                    <div class="col-3">
+                    <div class="col-12 col-md-6 col-lg-3">
                         <img src="${category.imgSrc}" alt="${category.description}" class="img-thumbnail">
                     </div>
                     <div class="col">
                         <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">${category.name}</h4>
+                            <h4 class="">${category.name}</h4>
                             <small class="text-muted">${category.productCount} artículos</small>
                         </div>
-                        <p class="mb-1">${category.description}</p>
+                        <p class="">${category.description}</p>
                     </div>
                 </div>
             </div>
